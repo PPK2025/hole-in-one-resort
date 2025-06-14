d1259e804365b67b46255dc45854539ca878bcf2
PPK <kimminppk@gmail.com>
2025-06-15 01:15:45 +0800

    Fix RedisStore import for connect-redis

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const session = require('express-session');
 const path = require('path');
 const bcrypt = require('bcrypt');
-const RedisStore = require('connect-redis').default;
+const RedisStore = require('connect-redis');
 const { createClient } = require('redis');
 
 const db = new sqlite3.Database('./holeinone.db');
